c6801a455b9d80962f026798401089aee21177ba
Suvradeep <suvradeep.paria829@gmail.com>
2020-09-07 01:30:20 +0530

    make it better

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -7,13 +7,15 @@ function RecipeList(props) {
             <div className="card">
             <div className="container">
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-12 col-lg-4 col-md-4">
                     <img src={props.image} alt="" />
                     </div>
                <div className="col-8">
-               <h2><b>{props.title}</b></h2>
-                <h4>Ingredients</h4> 
+                <div className="content-header">
+                <h4><b>{props.title}</b></h4>
+                </div>
                 <ol>
+                <h5>Ingredients</h5>
                 {props.ingredient.map( (process, index) => (
                     <li key={index}>{process.text}</li>
                 ))}
